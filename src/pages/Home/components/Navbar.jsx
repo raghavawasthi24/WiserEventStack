77f035a5c44ee847802a46db0ef2a4ae47371d24
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-01-25 15:07:46 +0530

    [home]:created reponsive navbar

---
+++ src/pages/Home/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='flex items-center gap-4 md:gap-12 justify-between px-4 py-2'>
+      <img src="assets/WiserCountLogo.png" alt="" className='w-[80px] md:w-[130px] h-[15px] md:h-[25px]'/>
+      <input
+        type="text"
+        className=" w-full border rounded-2xl indent-2 px-2 py-1 outline-none bg-blue-50" placeholder='RecentEventID'
+      />
+      <div className='flex gap-1'>
+        <img src="assets/profile.png" alt="" className='w-[40px]'/>
+        <div className='sm:flex flex-col mr-6 hidden'>
+          <p className='text-xs'>Test User</p>
+          <p className='text-xs'>abc@work.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
